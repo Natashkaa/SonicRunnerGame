132b6467f82a5cc1237008921e56630f6f9d3eae
Natalie Pryadko <natasha.cat982404@gmail.com>
2025-02-15 01:01:25 +0200

    Finished Sonic Runner game by YT tutorial

---
+++ src/entities/sonic.js
@@ -0,0 +1,35 @@
+import k from "../kaplayCtx";
+
+export function makeSonic(pos){
+    const sonic = k.add([
+        k.sprite("sonic", { anim: "run"}),
+        k.scale(4),
+        k.area(),
+        k.anchor("center"),
+        k.pos(pos),
+        k.body({ jumpForce: 1700 }),
+        {
+            setControls(){
+                k.onButtonPress("jump", () => {
+                    if(this.isGrounded()){
+                        this.play("jump");//play animation
+                        this.jump();
+                        k.play("jump", { volume: 0.5 });//play sound. it is global
+                    }
+                });
+            },
+            setEvents() {
+                this.onGround(() => {
+                    this.play("run");
+                });
+            }
+        }
+    ]);
+    sonic.ringCollectUI = sonic.add([
+        k.text("", { font: "mania", size: 24 }),
+        k.color(255,255,0),
+        k.anchor("center"),
+        k.pos(30,-10)
+    ])
+    return sonic;
+}
